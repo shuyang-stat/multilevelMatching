86c3ddada39f999a257474f7845322d9dd4cacee
BarkleyBG <BarkleyBG@users.noreply.github.com>
2018-07-21 12:54:06 -0500

    document the package itself

---
+++ R/multilevelMatching.R
@@ -0,0 +1,18 @@
+#' Propensity Score Matching and Subclassification in Observational Studies with
+#' Multi-level Treatments
+#'
+#' \pkg{multilevelMatching} implements the estimators introduced in Yang et al.
+#' (2016) \emph{Propensity score matching and subclassification in observational
+#' studies with multi‐level treatments}:
+#' \url{https://onlinelibrary.wiley.com/doi/abs/10.1111/biom.12505}. These are
+#' covariate- and propensity score-matching estimators for estimating the causal
+#' effect of multilevel treatment (i.e., 3 or more treatment types).
+#'
+#' For example, see \code{\link{multilevelMatchX}}.
+#'
+#' @name multilevelMatching
+#' @docType package
+NULL
+
+
+
